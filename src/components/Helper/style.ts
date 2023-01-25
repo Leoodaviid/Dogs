109970dfa8dc745c25276b6cd6c15aefc13d3e33
Leoodaviid <david_ax7@hotmail.com>
2023-01-25 15:30:54 -0300

    Criando animaçao com svg no loading

--- src/components/Helper/style.ts
+++ src/components/Helper/style.ts
@@ -29,4 +29,26 @@ const ImageSkeleton = styled.div`
   } ;
 `;
 
-export { ImageSkeleton };
+const LoadingStyle = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+
+  .loading {
+    margin: auto;
+    width: 80px;
+    height: 80px;
+    padding-left: 5px;
+    background: rgba(255, 255, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+  }
+`;
+
+export { ImageSkeleton, LoadingStyle };

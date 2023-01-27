b80fa34532ecfd476a1f6fa4c8b1e995da51a7c0
Leoodaviid <david_ax7@hotmail.com>
2023-01-27 20:50:14 -0300

    Retirando funçao getPhotos

--- src/Hooks/usePhoto.tsx
+++ src/Hooks/usePhoto.tsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
 import { Comment, Photo } from "../models/models";
-import {
-  PHOTOS_GET,
-  PHOTO_DELETE,
-  PHOTO_GET,
-  PHOTO_POST,
-} from "../services/MainApi/login";
+import { PHOTO_DELETE, PHOTO_GET, PHOTO_POST } from "../services/MainApi/login";
 
 interface ContextPhoto {
   error?: null;
   loading?: boolean;
   sendNewPhoto: (formData: FormData, token: any) => Promise<void>;
-  getPhotos: (page: number, total: number, user: number) => Promise<void>;
-  getPhoto: (id: string | number) => Promise<void>;
+  getPhoto: (id: string | number | undefined) => Promise<void>;
   photoDelete: (id: string) => Promise<void>;
   data: {
     formData?: FormData;
@@ -49,22 +43,8 @@ const usePhoto = (): ContextPhoto => {
       setLoading(false);
     }
   }
-  async function getPhotos(page: number, total: number, user: number) {
-    try {
-      setError(null);
-      setLoading(true);
-      const response = await PHOTOS_GET({ page, total, user });
-      if (response.status !== 200)
-        throw new Error(`Error: ${response.statusText}`);
-      setPhotoData(response.data);
-      console.log(response.data);
-    } catch (err: any) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-  }
-  async function getPhoto(id: string | number) {
+
+  async function getPhoto(id: string | number | undefined) {
     try {
       setError(null);
       setLoading(true);
@@ -102,7 +82,6 @@ const usePhoto = (): ContextPhoto => {
     photoDeleteData,
     setPhotoData,
     sendNewPhoto,
-    getPhotos,
     getPhoto,
     loading,
     error,

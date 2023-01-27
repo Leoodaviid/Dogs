777e2333d465dc38c059c8a9f88be973af450823
Leoodaviid <david_ax7@hotmail.com>
2023-01-27 20:51:22 -0300

    Criando componente de foto especifica pelo ID

---
+++ src/components/Photo/Photo.tsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import usePhoto from "../../Hooks/usePhoto";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "./PhotoContent";
+
+const Photo = () => {
+  const { getPhoto, photoComments, error, loading } = usePhoto();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getPhoto(id);
+  }, [id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (photoComments)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent single={true} data={photoComments} />
+      </section>
+    );
+  else return null;
+};
+
+export default Photo;

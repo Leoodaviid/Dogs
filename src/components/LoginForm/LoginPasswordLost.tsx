289dbbe7eaa6c53d28e4c91877b850ec4ed877fb
Leoodaviid <david_ax7@hotmail.com>
2023-01-28 13:30:16 -0300

    Criando pagina de recuperar senha

--- src/components/LoginForm/LoginPasswordLost.tsx
+++ src/components/LoginForm/LoginPasswordLost.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { FormEvent } from "react";
+import useApi from "../../Hooks/useApi";
+import useForm from "../../Hooks/useForm";
+import Botao from "../Button/Button";
+import Error from "../Helper/Error";
+import Input from "../Input/Input";
 
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = useForm("nome");
+  const { PasswordLost, error, loading } = useApi();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (login.validate()) {
+      PasswordLost({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+    }
+  }
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="email" {...login} />
+        {loading ? (
+          <Botao disabled>Enviando...</Botao>
+        ) : (
+          <Botao>Enviar Email</Botao>
+        )}
+      </form>
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
